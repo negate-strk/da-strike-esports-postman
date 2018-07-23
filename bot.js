e6544b258a51b0e34fbb15b8ff37fb24d2454b21
negate-strk <41571986+negate-strk@users.noreply.github.com>
2018-07-23 11:01:13 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,9 @@
+{
+  "token": "NDY4MDY5NDIwOTIxNTIwMTM4.DizzxQ.Zi7XASpRzdlPVovOne_vqZwgCmM",
+  "mailGuildId": "468064499887374376",
+  "mainGuildId": "285207922575933440",
+  "logChannelId": "id of the channel on the inbox server where notifications of new logs etc. will be posted",
+
+  "status": "Message me for help!",
+  "responseMessage": "Thank you for your message! Our staff team will reply to you here as soon as possible within the next 24 hours."
+}
